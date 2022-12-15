02af1d50a993078120f378dbb87342cf3e477e8a
ilovemove <77887500+ilovemove@users.noreply.github.com>
2022-12-15 21:11:25 +0200

    Update SingleLinkedList.cpp

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-#include "string"
 #include <cstdlib>
-using namespace std;
 
 template<typename T>
 class List
@@ -10,9 +8,14 @@ public:
 	List();
 	~List();
 	void push_back(T data);
-	int GetSize() { return Size; }
+	int GetSize() { return Size; };
 	T& operator[](const int index);
-	
+	void pop_front();
+	void clear();
+	void push_front(T data);
+	void pop_back();
+	void insert_at(T value, int index);
+	void remove_at(int index);
 private:
 	template<typename T>
 	class Node
@@ -36,9 +39,11 @@ List<T>::List()
 	Size = 0;
 	head = nullptr;
 }
+
 template<typename T>
 List<T>::~List()
 {
+	this->clear();
 }
 
 template<typename T>
@@ -59,6 +64,7 @@ void List<T>::push_back(T data)
 	}
 	Size++;
 }
+
 template<typename T>
 T& List<T>::operator[](const int index)
 {
@@ -75,17 +81,112 @@ T& List<T>::operator[](const int index)
 	}
 }
 
+template<typename T>
+void List<T>::pop_front()
+{
+	if (head != nullptr)
+	{
+	Node<T>* temp = head->pNext;
+	delete head;
+	Size--;
+	head = temp;
+	}
+}
+
+template<typename T>
+void List<T>::clear()
+{
+	while (this->Size)
+	{
+		this->pop_front();
+	}
+}
+
+template<typename T>
+void List<T>::push_front(T data)
+{
+	head = new Node<T>(data, head);
+	Size++;
+}
+
+template<typename T>
+void List<T>::pop_back()
+{
+	if (head != nullptr)
+	{
+		Node<T>* current = this->head;
+		while (current->pNext != nullptr)
+		{
+			current = current->pNext;
+		}
+		delete current;
+		Size--;
+	}
+}
+
+template<typename T>
+void List<T>::insert_at(T data, int index)
+{
+	if (index > Size)
+	{
+		push_back(data);
+	}
+	else 
+	{
+		Node<T>* previous = this->head;
+		for (int i = 0; i < index - 1; i++)
+		{
+			previous = previous->pNext;
+		}
+		previous->pNext = new Node<T>(data, previous->pNext);
+		Size++;
+	}
+}
+
+template<typename T>
+void List<T>::remove_at(int index)
+{
+	if (index > Size)
+	{
+		pop_back();
+	}
+	else
+	{
+		Node<T>* previous = this->head;
+		for (int i = 0; i < index - 1; i++)
+		{
+			previous = previous->pNext;
+		}
+		Node<T>* adress_to_delete = previous->pNext; 
+		Node<T>* next_adress = adress_to_delete->pNext; 
+		delete previous->pNext;
+		previous->pNext = next_adress;	
+		Size--;
+	}
+}
+
 int main()
 {
 	List<int> lst;
-	int numbers_count;
-	cout << "enter numbers count:";
-	cin >> numbers_count;
-	for (int i = 0; i < numbers_count; i++)
+	for (int i = 0; i < 5; i++)
 	{
 		lst.push_back(rand()%100);
-		cout << lst[i]  << endl;
+		std::cout << lst[i]  << std::endl;
 	}
+	std::cout  << "size = " << lst.GetSize()<< std::endl;
 
+	//lst.insert_at(444, 3);
+	//lst.insert_at(666, 14);
+	//lst.insert_at(777, 3);
+	lst.remove_at(2);
+	lst.remove_at(5);
+
+	std::cout << "-----After manipulations-----" << std::endl;
+
+	for (int i = 0; i < lst.GetSize(); i++)
+	{
+		std::cout << lst[i] << std::endl;
+	}
+	std::cout  << "size = " << lst.GetSize() << std::endl;
 	return 0;
 }
